1b30a20889d56a7abcb172c3882bd59925fc97fb
jashandhillon1 <jashandhillon211@gmail.com>
2024-03-23 21:50:28 -0400

    view product and edit product view

--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required(login_url='loginVendor')
+def inventory(request, pk):
+    page = 'inventory'
+    user_role = 'vendor'
+
+    shop_p = shop.objects.get(shop_id=pk)
+    products, search_query = searchproducts(request, pk)
+    return render(request, 'vendor/shopview_inventory.html',
+                  {'page': page, 'shop': shop_p, 'products': products, 'search_query': search_query,
+                   'user_role': user_role})
+
+def shopview(request, pk):
+    page = 'shopview'
+
+    if request.user.is_authenticated:
+        try:
+            user_consumer = Customer.objects.get(username=request.user.username)
+            user_role = 'customer'
+        except:
+            user_consumer = vendor.objects.get(username=request.user.username)
+            user_role = 'vendor'
+        print(user_consumer)
+    print(user_role)
+
+    shop_n = shop.objects.get(shop_id=pk)
+    products, search_query = searchproducts(request, pk)
+    return render(request, 'vendor/shopview_inventory.html', {'page':page, 'shop':shop_n, 'products':products, 'search_query':search_query, 'user_role': user_role})
+
