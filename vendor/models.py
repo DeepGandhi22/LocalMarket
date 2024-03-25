da2bace36239ab2ea106c1461b3a4a3df3a8c872
Het Patel <119762648+HP17-11@users.noreply.github.com>
2024-03-25 04:22:27 -0400

    Final_Project_With DATA
    Co-Authored-By: AeshaMehta18 <134636327+AeshaMehta18@users.noreply.github.com>
    Co-Authored-By: Devam22 <106398198+Devam22@users.noreply.github.com>
    Co-Authored-By: DeepGandhi22 <136626405+DeepGandhi22@users.noreply.github.com>
    Co-Authored-By: jashanpreet singh <45794461+jashandhillon1@users.noreply.github.com>
    Co-Authored-By: Neel Solanki <90170121+neelsolanki29@users.noreply.github.com>
    Co-Authored-By: Shivraj Mangaroliya <136628709+ShivrajMangaroliya1@users.noreply.github.com>

--- vendor/models.py
+++ vendor/models.py
@@ -21,6 +21,8 @@ class shop(models.Model):
     shop_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     gst_no = models.CharField(max_length=200, default=None)
     shop_name = models.CharField(max_length=200)
+    shop_image = models.ImageField(
+        null=True, blank=True, upload_to='shoppics/', default="shoppics/groceryShop.jpeg")
     vendor_id = models.ForeignKey(vendor, on_delete=models.CASCADE)
     shop_address = models.CharField(max_length=200, default=None)
     shop_city = models.CharField(max_length=100, default=None)
